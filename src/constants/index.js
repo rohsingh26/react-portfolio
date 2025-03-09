3dabc096cf572d9097dcc6481e0afb9382189a83
Rohit Kumar Singh <rohitsingh60881@gmail.com>
2025-03-09 19:42:11 +0530

    Added todo app project

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@ import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.png";
 import project5 from "../assets/projects/project-5.jpg";
+import project6 from "../assets/projects/project-6.png";
 
 export const HERO_CONTENT = `I am a passionate full-stack developer with expertise in front-end technologies like React, Vue.js, and Tailwind CSS, and back-end technologies such as Node.js, Express.js, MongoDB, Django, SQL, and Python. I use Git and GitHub as version control systems to ensure efficient collaboration and streamlined development processes. Additionally, I have experience with Google Cloud Platform (GCP) for deploying and managing cloud-based applications. This diverse skill set enables me to design and build robust, scalable, and efficient server-side solutions. My proficiency in C++ strengthens my problem-solving abilities, allowing me to approach challenges with precision and creativity. My goal is to develop innovative applications that drive business growth and deliver exceptional user experiences.`;
 
@@ -34,31 +35,38 @@ export const PROJECTS = [
     technologies: ["React", "Node.js", "Express.js", "MongoDB",],
   },
   {
-    title: "BIT Discuss",
+    title: "BIT Discuss - Web Portal",
     image: project2,
     description:
       "Developed BIT Discuss, a Twitter-inspired web application for college students, using Django for the back-end, SQLite for the database, and HTML/CSS for the front-end. The platform enables juniors and seniors to post doubts or discussion topics, fostering interaction and collaboration. Other users can comment on posts, providing insights and solutions.",
     technologies: ["Python", "Django","SQLite", "HTML", "CSS",],
   },
   {
-    title: "Weather Forecasting App",
-    image: project3,
-    description:
-      "Created a dynamic weather forecasting web app using HTML, CSS, and JavaScript, integrating the OpenWeather API for real-time weather updates. The app provides users with accurate weather details such as temperature, humidity, and conditions for any location.",
-    technologies: ["HTML", "CSS", "JavaScript", "API",],
-  },
-  {
     title: "Object Recognition System",
     image: project5,
     description:
-      "Built an Object Recognition System using the ESP32-CAM module for live video streaming and real-time object detection. Programmed the ESP32 microcontroller in C++ and utilized Python for object recognition. Used NumPy to convert bit arrays into images and OpenCV datasets for object detection. Implemented multi-threading to display live streaming and detection outputs in separate windows, ensuring efficient concurrent processing.",
+      "Built an Object Recognition System using the ESP32-CAM module for live video streaming and real-time object detection. Programmed the ESP32 microcontroller in C++ and used Python for object recognition. Employed NumPy to convert bit arrays into images and leveraged OpenCV datasets along with YOLOv3, a pre-trained machine learning algorithm, for object detection. Implemented multi-threading to display live streaming and detection outputs in separate windows, ensuring efficient concurrent processing.",
     technologies: ["Python", "C++", "OpenCV", "NumPy",],
+  },
+  {
+    title: "To-Do App",
+    image: project6,
+    description:
+      "Developed a dynamic To-Do App using Vue.js, enabling users to create, edit, and delete tasks categorized as Personal or Professional. The app allows users to input their name and provides a user-friendly interface to manage tasks efficiently. Tasks can be marked as completed by clicking on them, which neatly strikes them out. Additionally, users can modify task names even after adding them, offering flexibility. A key feature is the use of local storage to save data directly on the device, ensuring tasks persist across sessions for a seamless and reliable user experience.",
+    technologies: ["Vue.js", "JavaScript", "CSS", "JSON"],
+  },
+  {
+    title: "Weather Forecasting App",
+    image: project3,
+    description:
+      "Built a dynamic weather forecasting web app using HTML, CSS, and JavaScript, integrating the OpenWeather API for real-time weather updates. The app provides users with accurate weather details such as temperature, humidity, and conditions for any location.",
+    technologies: ["HTML", "CSS", "JavaScript", "API",],
   },
   {
     title: "QuickCart - An E-Commerce Website",
     image: project4,
     description:
-      "Designed Quick Cart, an e-commerce website using HTML, CSS, and JavaScript. This project focuses on an intuitive front-end design, featuring a responsive layout, product listings, and interactive features like cart management. Quick Cart demonstrates my ability to build user-friendly and visually appealing web interfaces tailored for e-commerce platforms",
+      "Designed QuickCart, an e-commerce website using HTML, CSS, and JavaScript. This project focuses on an intuitive front-end design, featuring a responsive layout, product listings, and interactive features like cart management. Quick Cart demonstrates my ability to build user-friendly and visually appealing web interfaces tailored for e-commerce platforms",
     technologies: ["HTML", "CSS", "JavaScript", "GitHub",],
   },
 ];
