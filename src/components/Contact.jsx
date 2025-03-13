ffe3af9c5fafca5385bb47ded2d85190b8c3be4e
Rohit Kumar Singh <rohitsingh60881@gmail.com>
2025-03-13 19:01:56 +0530

    Faster the tech

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
       <motion.h2
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration:0.5}}
         className="my-10 text-center text-4xl"
       >
         Get in Touch
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -45,7 +45,7 @@ const Technologies = () => {
       <motion.h2 
       whileInView={{opacity:1, y:0}}
       initial={{opacity:0, y: -100}}
-      transition={{duration:1.5}}
+      transition={{duration:0.5}}
       className="my-20 text-center text-4xl">Technologies</motion.h2>
       <motion.div whileInView={{opacity:1, x:0}}
       initial={{opacity:0, x:-100}}
