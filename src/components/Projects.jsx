20bbd81d28cc3ef06351afed9c5d1b4056d2608b
Rohit Kumar Singh <rohitsingh60881@gmail.com>
2025-03-17 17:41:01 +0530

    Added links to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,36 +1,74 @@
 import { PROJECTS } from "../constants";
 import { motion } from "framer-motion";
+
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
       <motion.h2 
-      whileInView={{ opacity:1, y:0}}
-      initial={{opacity:0, y:-100}}
-      transition={{duration:0.5}}
-      className="my-20 text-center text-4xl">Projects</motion.h2>
+        whileInView={{ opacity:1, y:0 }}
+        initial={{ opacity:0, y:-100 }}
+        transition={{ duration:0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h2>
       <div>
         {PROJECTS.map((project, index) => (
-            <div key={index} className="mb-12 flex flex-wrap lg:justify-center">
-                <motion.div
-                whileInView={{ opacity:1, x:0}}
-                initial={{opacity:0, x:-100}}
-                transition={{duration:1}} className="w-full lg:w-1/4">
-                <img src={project.image} width={200} height={200} alt={project.title} className="mb-6 rounded"/>
-                </motion.div>
-                <motion.div
-                whileInView={{ opacity:1, x:0}}
-                initial={{opacity:0, x:100}}
-                transition={{duration:1}}
-                className="w-full max-w-xl lg:w-3/4">
-                    <h5 className="mb-2 font-semibold">{project.title}</h5>
-                    <p className="mb-4 text-neutral-400 text-justify w-full">{project.description}</p>
-                    {project.technologies.map((tech, index) => (
-                        <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-700">
-                            {tech}
-                        </span>
-                    ))}
-                </motion.div>
-            </div>
+          <div key={index} className="mb-12 flex flex-wrap lg:justify-center">
+            <motion.div
+              whileInView={{ opacity:1, x:0 }}
+              initial={{ opacity:0, x:-100 }}
+              transition={{ duration:1 }}
+              className="w-full lg:w-1/4"
+            >
+              <img 
+                src={project.image} 
+                width={200} 
+                height={200} 
+                alt={project.title} 
+                className="mb-6 rounded" 
+              />
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity:1, x:0 }}
+              initial={{ opacity:0, x:100 }}
+              transition={{ duration:1 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
+              <h5 className="mb-2 font-semibold">{project.title}</h5>
+              <p className="mb-4 text-neutral-400 text-justify w-full">{project.description}</p>
+              <div className="mb-4">
+                {project.technologies.map((tech, index) => (
+                  <span 
+                    key={index} 
+                    className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-700"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex space-x-4">
+                <a 
+                  href={project.repo} 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="bg-stone-300 hover:bg-stone-400 text-stone-800 px-2 py-1 text-xs rounded shadow-sm transition-all"
+                >
+                  Link to Repository
+                </a>
+                {project.liveDemo && (
+                <a 
+                  href={project.liveDemo} 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="bg-green-200 hover:bg-green-300 text-green-800 px-2 py-1 text-xs rounded shadow-sm transition-all"
+                >
+                  Live Demo
+                </a>
+                )}
+              </div>
+            </motion.div>
+          </div>
         ))}
       </div>
     </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,8 @@ import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.png";
 import project5 from "../assets/projects/project-5.jpg";
 import project6 from "../assets/projects/project-6.png";
+import project7 from "../assets/projects/project-7.png";
+import project8 from "../assets/projects/project-8.png";
 
 export const HERO_CONTENT = `I am a passionate full-stack developer with expertise in front-end technologies like React, Vue.js, and Tailwind CSS, and back-end technologies such as Node.js, Express.js, MongoDB, Django, SQL, and Python. I use Git and GitHub as version control systems to ensure efficient collaboration and streamlined development processes. Additionally, I have experience with Google Cloud Platform (GCP) for deploying and managing cloud-based applications. This diverse skill set enables me to design and build robust, scalable, and efficient server-side solutions. My proficiency in C++ strengthens my problem-solving abilities, allowing me to approach challenges with precision and creativity. My goal is to develop innovative applications that drive business growth and deliver exceptional user experiences.`;
 
@@ -33,6 +35,7 @@ export const PROJECTS = [
     description:
       "Developed BIT Connect, a real-time chat web application for college students using the MERN stack (MongoDB, Express.js, React.js, Node.js) and Socket.io for seamless real-time communication. This platform enables juniors and seniors to interact, ask questions, and resolve doubts effectively. The app features an user-friendly interface, instant messaging, and scalability to handle multiple users simultaneously.",
     technologies: ["React", "Node.js", "Express.js", "MongoDB",],
+    repo: "https://github.com/rohsingh26/bitconnect",
   },
   {
     title: "BIT Discuss - Web Portal",
@@ -40,6 +43,25 @@ export const PROJECTS = [
     description:
       "Developed BIT Discuss, a Twitter-inspired web application for college students, using Django for the back-end, SQLite for the database, and HTML/CSS for the front-end. The platform enables juniors and seniors to post doubts or discussion topics, fostering interaction and collaboration. Other users can comment on posts, providing insights and solutions.",
     technologies: ["Python", "Django","SQLite", "HTML", "CSS",],
+    repo: "https://github.com/rohsingh26/bitdiscuss",
+  },
+  {
+    title: "To-Do App",
+    image: project6,
+    description:
+      "Developed a dynamic To-Do App using Vue.js, enabling users to create, edit, and delete tasks categorized as Personal or Professional. The app allows users to input their name and provides a user-friendly interface to manage tasks efficiently. Tasks can be marked as completed by clicking on them, which neatly strikes them out. Additionally, users can modify task names even after adding them, offering flexibility. A key feature is the use of local storage to save data directly on the device, ensuring tasks persist across sessions for a seamless and reliable user experience.",
+    technologies: ["Vue.js", "JavaScript", "CSS", "JSON",],
+    repo: "https://github.com/rohsingh26/todo-app",
+    liveDemo: "https://todo-app-rohit-singhs-projects-86330224.vercel.app/",
+  },
+  {
+    title: "Expense Tracker",
+    image: project7,
+    description:
+      "Built a simple yet effective Expense Tracker Web App using Vue.js to help users manage their daily spending. Users can set a daily budget limit, track expenses, and get instant feedback on overspending or savings. The app stores data locally, ensuring expenses aren’t lost even after refreshing. A 'Clear All Data' button allows instant reset. Designed with a clean UI for easy and efficient tracking.",
+    technologies: ["Vue.js", "JavaScript", "CSS", "JSON",],
+    repo: "https://github.com/rohsingh26/Expense-Tracker",
+    liveDemo: "https://expense-tracker-rohit-singhs-projects-86330224.vercel.app/",
   },
   {
     title: "Object Recognition System",
@@ -47,13 +69,7 @@ export const PROJECTS = [
     description:
       "Built an Object Recognition System using the ESP32-CAM module for live video streaming and real-time object detection. Programmed the ESP32 microcontroller in C++ and used Python for object recognition. Employed NumPy to convert bit arrays into images and leveraged OpenCV datasets along with YOLOv3, a pre-trained machine learning algorithm, for object detection. Implemented multi-threading to display live streaming and detection outputs in separate windows, ensuring efficient concurrent processing.",
     technologies: ["Python", "C++", "OpenCV", "NumPy",],
-  },
-  {
-    title: "To-Do App",
-    image: project6,
-    description:
-      "Developed a dynamic To-Do App using Vue.js, enabling users to create, edit, and delete tasks categorized as Personal or Professional. The app allows users to input their name and provides a user-friendly interface to manage tasks efficiently. Tasks can be marked as completed by clicking on them, which neatly strikes them out. Additionally, users can modify task names even after adding them, offering flexibility. A key feature is the use of local storage to save data directly on the device, ensuring tasks persist across sessions for a seamless and reliable user experience.",
-    technologies: ["Vue.js", "JavaScript", "CSS", "JSON"],
+    repo: "https://github.com/rohsingh26/Object-Recognisition",
   },
   {
     title: "Weather Forecasting App",
@@ -61,6 +77,17 @@ export const PROJECTS = [
     description:
       "Built a dynamic weather forecasting web app using HTML, CSS, and JavaScript, integrating the OpenWeather API for real-time weather updates. The app provides users with accurate weather details such as temperature, humidity, and conditions for any location.",
     technologies: ["HTML", "CSS", "JavaScript", "API",],
+    repo: "https://github.com/rohsingh26/Weather-App",
+    liveDemo: "https://rohsingh26.github.io/Weather-App/",
+  },
+  {
+    title: "Basic Calculator Web App",
+    image: project8,
+    description:
+      "Created a Basic Calculator using HTML, CSS, and JavaScript, allowing users to perform addition, subtraction, multiplication, and division. It features a simple and responsive UI, making calculations quick and easy. This project showcases my skills in frontend development and JavaScript functionality for handling arithmetic operations.",
+    technologies: ["HTML", "CSS", "JavaScript", "GitHub",],
+    repo: "https://github.com/rohsingh26/Calculator",
+    liveDemo: "https://rohsingh26.github.io/Calculator/",
   },
   {
     title: "QuickCart - An E-Commerce Website",
@@ -68,6 +95,8 @@ export const PROJECTS = [
     description:
       "Designed QuickCart, an e-commerce website using HTML, CSS, and JavaScript. This project focuses on an intuitive front-end design, featuring a responsive layout, product listings, and interactive features like cart management. Quick Cart demonstrates my ability to build user-friendly and visually appealing web interfaces tailored for e-commerce platforms",
     technologies: ["HTML", "CSS", "JavaScript", "GitHub",],
+    repo: "https://github.com/rohsingh26/E-Comm",
+    liveDemo: "https://rohsingh26.github.io/E-Comm/",
   },
 ];
 
